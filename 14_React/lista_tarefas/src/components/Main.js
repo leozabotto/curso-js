68e791375cc58a55e640bd0d1294df954da6cf45
Leonardo <leozabotto02@gmail.com>
2020-12-05 18:21:34 -0300

    Atualização do Repositório

---
+++ 14_React/lista_tarefas/src/components/Main.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+import './Main.css';
+
+export default class Main extends Component {
+  state = {
+    novaTarefa: '',
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      novaTarefa: e.target.value,
+    });
+  }
+
+  render() {
+    const { novaTarefa } = this.state;
+    return (
+      <div className="main">
+        <h1>{novaTarefa}</h1>
+
+        <form action="#">
+          <input type="text" onChange={this.handleChange} />
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+    );
+  }
+}
--- divisao.js
+++
@@ -1,6 +0,0 @@
-function divisao(n1, n2) {
-  const resultado = n1 / n2
-  console.log(resultado)
-}
-
-divisao(7, 16)
